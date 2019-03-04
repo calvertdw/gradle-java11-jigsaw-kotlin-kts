b077cf78f709aecf54dd47d5c834fec62f009e9a
Duncan Calvert <dcalvert@ihmc.us>
2019-03-03 22:40:20 -0600

    Start on jigsaw plugin

--- buildSrc/src/main/kotlin/com/example/JigsawPlugin.kt
+++ buildSrc/src/main/kotlin/com/example/JigsawPlugin.kt
@@ -6,6 +6,7 @@ import java.io.BufferedReader
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
 import java.util.stream.Stream
 
 class JigsawPlugin : Plugin<Project>
@@ -18,19 +19,22 @@ class JigsawPlugin : Plugin<Project>
                val buildLibsDir = project.buildDir.resolve("libs").toPath()
                Files.createDirectories(buildLibsDir)
 
+               val str = "An 😀awesome 😃string with a few 😉emojis!"
+
                val fileListStream = Files.list(buildLibsDir).parallel()
 
                var javaHome = Paths.get(System.getProperty("java.home"))
                var modulesHome = Paths.get(System.getProperty("java.home"))
 
-//               fileListStream.flatMap<String> {
-//                  var jdepsCommand = arrayOf(javaHome.resolve("bin/java").toString(),
-//                                             "--list-modules",
-//                                             "--module-path",
-//                                             modulesHome.resolve("jmods").toString())
-//
-//                  BufferedReader
-//               }
+               fileListStream.flatMap<String> {
+                  var jdepsCommand = arrayOf(javaHome.resolve("bin/java").toString(),
+                                             "--list-modules",
+                                             "--module-path",
+                                             modulesHome.resolve("jmods").toString())
+
+                  val commandProcess = Runtime.getRuntime().exec(jdepsCommand);
+                  commandProcess.waitFor(20, TimeUnit.MINUTES)
+               }
          }
       }
    }
